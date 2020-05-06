8334a48375205fc203172351b0f113cf7aa4a40b
wu <2295804511@qq.com>
2020-05-06 17:01:25 +0800

    add toString&main method

--- src/problems/dataStructure/linkedList/DesignLinkedList_707.java
+++ src/problems/dataStructure/linkedList/DesignLinkedList_707.java
@@ -5,7 +5,12 @@ package problems.dataStructure.linkedList;
  * @Date: 2020/3/8
  * @Description: 设计链表的实现。可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val和next。val是当前节点的值，next是指向
  * 下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性prev以指示链表中的上一个节点。假设链表中的所有节点都是0-index的。
- *
+ * 在链表中需要实现这些功能：
+ *     get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+ *     addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+ *     addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+ *     addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+ *     deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
  */
 public class DesignLinkedList_707 {
 
@@ -93,7 +98,37 @@ public class DesignLinkedList_707 {
             current.next = current.next.next;
             length--;
         }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            Node current = head.next;
+            for (int i = 0; i < length; i++ ){
+                if (current.next != null){
+                    sb.append(current.val).append(" -> ");
+                    current = current.next;
+                } else {
+                    sb.append(current.val);
+                }
+            }
+            sb.append("]");
+            return sb.toString();
+        }
     }
 
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new DesignLinkedList_707().new MyLinkedList();
+        linkedList.addAtHead(1);
+        linkedList.addAtTail(3);
+        linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
 
+        System.out.println(linkedList);
+
+        System.out.println(linkedList.get(1));   //返回2
+        linkedList.deleteAtIndex(1);             //现在链表是1-> 3
+
+        System.out.println(linkedList.get(1));   //返回3
+        System.out.println(linkedList);
+    }
 }
