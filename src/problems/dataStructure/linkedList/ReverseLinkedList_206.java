45a3306c414d89d994bcc2055c70b7946d50b6d3
wu <2295804511@qq.com>
2020-05-31 22:32:57 +0800

    206

--- src/problems/dataStructure/linkedList/ReverseLinkedList_206.java
+++ src/problems/dataStructure/linkedList/ReverseLinkedList_206.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 /**
  * @User: 吴广谋
  * @Date: 2020/5/27
- * @Description: 反转一个单链表。
+ * @Description: 反转一个单链表
  * 示例：输入：1->2->3->4->5->NULL     输出：5->4->3->2->1->NULL
  */
 public class ReverseLinkedList_206 {
