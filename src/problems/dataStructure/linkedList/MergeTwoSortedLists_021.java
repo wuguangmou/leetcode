85670e819a808ddc19f5de1a863b4c7726ba9334
wu <2295804511@qq.com>
2021-03-07 22:18:58 +0800

    更改21文件名称

--- src/problems/dataStructure/linkedList/MergeTwoSortedLists_021.java
+++ src/problems/dataStructure/linkedList/MergeTwoSortedLists_021.java
@@ -6,7 +6,7 @@ package problems.dataStructure.linkedList;
  * @Description: 将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
  * 例：输入：1->2->4, 1->3->4     输出：1->1->2->3->4->4
  */
-public class MergeTwoSortedLists_21 {
+public class MergeTwoSortedLists_021 {
     static class ListNode{
         int val;
         ListNode next;
